e178ee8bb50a6d78898b6b435f645f8db45a910a
nikfarjam <mm.nikfarjam@gmail.com>
2025-08-26 23:12:19 +1000

    Init Converter API

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
-# unit-convertor-go
-An example serverless RESTful API in GoLang
+# Unit Convertor
+
+An example serverless RESTful API written in GoLang
+
+## Overview
+
+This project is a simple unit conversion service that provides an API for converting temperature units. It's designed to be lightweight and easy to deploy as a serverless application.
+
+## Features
+
+- Converts between Fahrenheit and Celsius units.
+- RESTful API endpoints.
+- Written in GoLang for performance and simplicity.
+
+## Getting Started
+
+### Prerequisites
+
+- [Go](https://golang.org/dl/) installed on your machine.
---
+++ main.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+	"strings"
+
+    "github.com/nikfarjam/unit-convertor-go/pkg/converter"
+)
+
+func main() {
+	http.HandleFunc("/converter", converterHandler)
+	addr := ":9090"
+	log.Printf("Server is running on http://localhost%s", addr)
+	if err := http.ListenAndServe(addr, nil); err != nil {
+		log.Fatal(err)
+	}
+}
+
+func converterHandler(w http.ResponseWriter, r *http.Request) {
+	defer r.Body.Close()
+	dec := json.NewDecoder(r.Body)
+	req := &converter.ConverterRequest{}
+
+	if err := dec.Decode(req); err != nil {
+		log.Printf("Error: bad request %s", err)
+		http.Error(w, "bad request", http.StatusBadRequest)
+		return
+	}
+
+	if strings.ToUpper(req.From) != "CELSIUS" && strings.ToUpper(req.From) != "FAHRENHEIT" {
+		log.Printf("Error: invalid unit")
+		http.Error(w, "invalid from", http.StatusBadRequest)
+		return
+	}
+
+	if strings.ToUpper(req.To) != "CELSIUS" && strings.ToUpper(req.To) != "FAHRENHEIT" {
+		log.Printf("Error: invalid unit")
+		http.Error(w, "invalid from", http.StatusBadRequest)
+		return
+	}
+
+	resp, err := converter.ConvertUnit(*req)
+	if err != nil {
+		log.Printf("not able to process request %v", err)
+		http.Error(w, "not able to process request", http.StatusBadRequest)
+		return
+	}
+
+	enc := json.NewEncoder(w)
+	if err := enc.Encode(resp); err != nil {
+		log.Printf("Error: not able to encode response %s", err)
+		http.Error(w, "not able to process request", http.StatusInternalServerError)
+		return
+	}
+}
